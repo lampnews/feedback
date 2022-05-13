d3814c8327b955c2727c63553c4228ef8b467541
Lamp News <88138329+lampnews@users.noreply.github.com>
2022-05-13 21:53:37 +0200

    Update formmail.php

--- formmail.php
+++ formmail.php
@@ -1,12 +1,35 @@
 <?php
-$miamail = "alupidi888@gmail.com";
-$successo = $_POST['successo'];
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$msg = $_POST['msg'];
-$messaggio = "Nome: " . $nome . "\nEmail: " . $email . "\nTel: " . $tel . "\nMessaggio: " . $msg;
-$headers = "From: " . $miamail . "\r\n" . "Reply-To: " . $miamail;
-mail($miamail,"Messaggio dal sito",$messaggio,$headers);
-header("location: " . $successo);
-?>  
+$adminEmail = 'alupidi888@gmail.com';
+$userEmail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$userMessage = '
+  <html>
+    <head>
+      <title>Grazie per averci contattato</title>
+    </head>
+    <body>
+      <h1>Grazie per averci contattato</h1>
+      <p>La tua richiesta è stata inoltrata. Ti risponderemo al più presto.</p>
+      <p>Lo Staff di Lamp News</p>
+    </body>
+  </html>
+';
+$adminMessage = "
+  <html>
+    <head>
+      <title>Contatto dal sito web</title>
+    </head>
+    <body>
+      <h1>Contatto dal sito web</h1>
+      <ul>
+        <li>Nome: {$_POST['name']}</li>
+        <li>Oggetto: {$_POST['subject']}</li>
+        <li>Messaggio: {$_POST['message']}</li>
+      </ul>
+    </body>
+  </html>
+";
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=utf-8';
+mail($userEmail, 'Richiesta di contatto effettuata con successo', $userMessage, implode("\r\n", $headers));
+mail($adminEmail, 'Richiesta di contatto dal sito web', $adminMessage, implode("\r\n", $headers));
+echo "Messaggio inviato con successo";
